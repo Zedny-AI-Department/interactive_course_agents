0dcf9b67bb9ea67846df2efbd4b27750ab889177
ola172 <olaemadzahran@gmail.com>
2025-09-15 18:03:19 +0300

    fix: add app middleware

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes import data_processing_router
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # أو ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(data_processing_router)
